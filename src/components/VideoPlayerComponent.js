23b2c62bbdb051d913d7adb5a1e2f6598ec23a38
avinashneradotminds <avinash@dotminds.in>
2022-06-05 14:48:13 +0530

    base skeleton for the UI

---
+++ src/components/VideoPlayerComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+function VideoPlayerComponent() {
+    return <ReactPlayer url="https://www.youtube.com/watch?v=TUxAZGt1pKw" width="100%"></ReactPlayer>;
+}
+
+export { VideoPlayerComponent };
